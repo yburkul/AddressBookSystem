379abea3b9cb7550af0021965e44cc71e394b7ec
yburkul <yburkul123@gmail.com>
2022-03-20 23:48:55 +0530

    Added:Edit Contact

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -65,6 +65,29 @@ namespace AddressBook
             contactDetailsList.Add(contactDetails);
             contactDetailsMap.Add(FirstName, contactDetails);
         }
+
+        public void EditDetails(string First_Name)
+        {
+            Console.WriteLine("Enter Your First name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter Your Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter Your City");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter Your State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter Your Zipcode");
+            int Zipcode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Phone number");
+            int PhoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Email Id");
+            string EmailId = Console.ReadLine();
+            ContactDetails contactDetails = new ContactDetails(FirstName, LastName, Address, City, State, Zipcode, PhoneNumber, EmailId);
+            contactDetailsList.Add(contactDetails);
+            contactDetailsMap[First_Name] = contactDetails;
+        }
         public void ComputeDetails()
         {
             foreach (ContactDetails contact in contactDetailsList)
@@ -79,9 +102,11 @@ namespace AddressBook
             Program details = new Program();
             do
             {
-                Console.WriteLine("1 : To Add a Contact Details");
-                Console.WriteLine("2: To Compute Details");
-            
+                Console.WriteLine("Choose 1: To Add a Contact Details");
+                Console.WriteLine("Choose 2: To Compute Details");
+                Console.WriteLine("Choose 3: To Edit a Contact Details");
+                //Console.WriteLine("Choose 0: To Exit");
+
                 try
                 {
                     option = int.Parse(Console.ReadLine());
@@ -93,6 +118,11 @@ namespace AddressBook
                         case 2:
                             details.ComputeDetails();
                             continue;
+                        case 3:
+                            Console.WriteLine("Enter a First Name to Edit");
+                            string First_Name = Console.ReadLine();
+                            details.EditDetails(First_Name);
+                            break;
                         default:
                             Console.WriteLine("Wrong key");
                             break;
